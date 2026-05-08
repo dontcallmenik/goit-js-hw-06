54fe830aee558d02ebcebf3a3d60131c0bc1f25a
dontcallmenik <dontcallmenik@gmail.com>
2026-05-08 20:35:49 +0300

    67
    67

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,35 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = firstArray.concat(secondArray);
-  if (newArray.length > maxLength) {
-    return newArray.slice(0, maxLength);
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
   }
-  return newArray;
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
 }
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems());
+
+storage.addItem('Droid');
+console.log(storage.getItems());
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems());
+
+storage.removeItem('Scaner');
+console.log(storage.getItems());
--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,26 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
-  for (const number of numbers) {
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
+class StringBuilder {
+  #value;
+  constructor(value) {
+    this.#value = value;
   }
-  return filteredNumbers;
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
